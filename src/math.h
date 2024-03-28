d6c3dc7072f04e261d9c2dc9022f9c47124fc9ae
nilsoncore <nilsonragee@gmail.com>
2024-03-28 10:46:26 +0300

    math: Implement `clamp`, `min2`, `max2`, `abs` functions.

--- src/math.h
+++ src/math.h
@@ -3,14 +3,62 @@
 
 #include "types.h"
 
+#undef min
+#undef max
+
 // Sections:
-// [SECTION] Forward declarations
+// [SECTION] Math functions
+// [SECTION] Vectors / Matrices forward declarations
 // [SECTION] Vector2: s8 -> u8 -> s16 -> u16 -> s32 -> u32 -> s64 -> u64 -> f32 -> f64
 // [SECTION] Vector3: s8 -> u8 -> s16 -> u16 -> s32 -> u32 -> s64 -> u64 -> f32 -> f64
 // [SECTION] Vector4: s8 -> u8 -> s16 -> u16 -> s32 -> u32 -> s64 -> u64 -> f32 -> f64
 
 //-----------------------------------------------------------------------------
-// [SECTION] Forward declarations
+// [SECTION] Math functions
+//-----------------------------------------------------------------------------
+
+s8 QL_clamp(s8 x, s8 min, s8 max);
+u8 QL_clamp(u8 x, u8 min, u8 max);
+s16 QL_clamp(s16 x, s16 min, s16 max);
+u16 QL_clamp(u16 x, u16 min, u16 max);
+s32 QL_clamp(s32 x, s32 min, s32 max);
+u32 QL_clamp(u32 x, u32 min, u32 max);
+s64 QL_clamp(s64 x, s64 min, s64 max);
+u64 QL_clamp(u64 x, u64 min, u64 max);
+f32 QL_clamp(f32 x, f32 min, f32 max);
+f64 QL_clamp(f64 x, f64 min, f64 max);
+
+s8 QL_min2(s8 a, s8 b);
+u8 QL_min2(u8 a, u8 b);
+s16 QL_min2(s16 a, s16 b);
+u16 QL_min2(u16 a, u16 b);
+s32 QL_min2(s32 a, s32 b);
+u32 QL_min2(u32 a, u32 b);
+s64 QL_min2(s64 a, s64 b);
+u64 QL_min2(u64 a, u64 b);
+f32 QL_min2(f32 a, f32 b);
+f64 QL_min2(f64 a, f64 b);
+
+s8 QL_max2(s8 a, s8 b);
+u8 QL_max2(u8 a, u8 b);
+s16 QL_max2(s16 a, s16 b);
+u16 QL_max2(u16 a, u16 b);
+s32 QL_max2(s32 a, s32 b);
+u32 QL_max2(u32 a, u32 b);
+s64 QL_max2(s64 a, s64 b);
+u64 QL_max2(u64 a, u64 b);
+f32 QL_max2(f32 a, f32 b);
+f64 QL_max2(f64 a, f64 b);
+
+s8 QL_abs(s8 x);
+s16 QL_abs(s16 x);
+s32 QL_abs(s32 x);
+s64 QL_abs(s64 x);
+f32 QL_abs(f32 x);
+f64 QL_abs(f64 x);
+
+//-----------------------------------------------------------------------------
+// [SECTION] Vectors/ Matrices forward declarations
 //-----------------------------------------------------------------------------
 
 struct Vector2_s8;
@@ -824,7 +872,7 @@ struct Vector4_s16 {
 	union { s16 y; s16 g; s16 height; };
 	union { s16 z; s16 b; s16 depth;  };
 	union { s16 w; s16 a;             };
-	
+
 	bool operator == (Vector4_s16 rhs);
 	bool operator == (s16 rhs);
 
@@ -860,7 +908,7 @@ struct Vector4_u16 {
 	union { u16 y; u16 g; u16 height; };
 	union { u16 z; u16 b; u16 depth;  };
 	union { u16 w; u16 a;             };
-		
+
 	bool operator == (Vector4_u16 rhs);
 	bool operator == (u16 rhs);
 
